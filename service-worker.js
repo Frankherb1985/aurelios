d9c3de4be8447f0c63ac9c0f90c0e7e73653238a
Frank Herbert <mr.frankherb@gmail.com>
2025-10-04 08:14:55 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 
 self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('aurelios-v1').then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.json'])))
+  e.waitUntil(caches.open('aurelios-v2').then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.json'])))
 })
 self.addEventListener('fetch', e=>{
   e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)))
