03f18cb5b24e1dd04ca35064e1a253710fb0f6ec
Frank Herbert <mr.frankherb@gmail.com>
2025-10-12 08:19:07 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,15 @@
-
+// one-time hard reset to clear old SW caches
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(async regs => {
+    for (const r of regs) {
+      try { await r.unregister(); } catch {}
+    }
+    if (caches && caches.keys) {
+      const keys = await caches.keys();
+      for (const k of keys) { try { await caches.delete(k); } catch {} }
+    }
+  });
+}
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open('aurelios-v2').then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.json'])))
 })
