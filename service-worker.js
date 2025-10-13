f361b94660432d009c7b4efbff63b40d9323cf17
Frank Herbert <mr.frankherb@gmail.com>
2025-10-13 13:36:27 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,27 @@
-const CACHE="aurelios-v20251015";
-const FILES=["/aurelios/","/aurelios/index.html","/aurelios/style.20251015.css","/aurelios/app.20251015.js","/aurelios/manifest.json"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));self.clients.claim();});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));});
+const CACHE = 'aurelios-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.20251013.css',
+  './app.20251013.js',
+  './ui.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+});
