0cb495794571906c45ed846475ea73ad2468d65e
Frank Herbert <mr.frankherb@gmail.com>
2025-10-13 12:44:52 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,5 @@
-// GitHub Pages (project site) scope: /aurelios/
-const VERSION = 'aurelios-v20251013-7';
-const ASSETS = [
-  '/aurelios/',
-  '/aurelios/index.html',
-  '/aurelios/style.20251013.css',
-  '/aurelios/app.20251013.js',
-  '/aurelios/ui.js',
-  '/aurelios/manifest.json'
-];
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(VERSION).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)));
-    await self.clients.claim();
-  })());
-});
-
-self.addEventListener('fetch', e => {
-  const req = e.request;
-  e.respondWith((async () => {
-    const cached = await caches.match(req);
-    if (cached) return cached;
-    const resp = await fetch(req);
-    if (req.method === 'GET' && new URL(req.url).origin === location.origin) {
-      const c = await caches.open(VERSION);
-      c.put(req, resp.clone());
-    }
-    return resp;
-  })());
-});
+const CACHE="aurelios-v20251015";
+const FILES=["/aurelios/","/aurelios/index.html","/aurelios/style.20251015.css","/aurelios/app.20251015.js","/aurelios/manifest.json"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));});
