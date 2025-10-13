a61c6af532e8ec5a5c7d9aa60cc2126515481cc1
Frank Herbert <mr.frankherb@gmail.com>
2025-10-13 15:52:58 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,24 @@
-const CACHE = "aurelios-v20251013";
+const CACHE = "aurelios-v4"; // bump this when you push
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./cinematic.css",
+  "./app.cinematic.js",
+  "./manifest.json"
+];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((c) =>
-      c.addAll([
-        "./",
-        "index.html",
-        "style.20251013.css",
-        "app.20251013.js",
-        "ui.js",
-        "manifest.json",
-        "icon-192.png",
-        "icon-512.png"
-      ])
-    )
-  );
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener("activate", e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then((resp) => {
-      return resp || fetch(event.request);
-    })
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
