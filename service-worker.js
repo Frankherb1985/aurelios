e087c15d306559a7cfb7e93b49ba824a3f78987a
Frank Herbert <mr.frankherb@gmail.com>
2025-10-13 15:25:10 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,36 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("aurelios-cache").then((cache) => {
-      return cache.addAll([
+const CACHE = "aurelios-v20251013";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((c) =>
+      c.addAll([
         "./",
         "index.html",
         "style.20251013.css",
         "app.20251013.js",
         "ui.js",
-        "manifest.json"
-      ]);
-    })
+        "manifest.json",
+        "icon-192.png",
+        "icon-512.png"
+      ])
+    )
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then((resp) => {
+      return resp || fetch(event.request);
+    })
   );
 });
